17831d14cb8d1fdb226f42ca4b052145b1e2d895
Gio-glitch04 <killerghast.2013@gmail.com>
2024-09-25 19:30:27 -0300

    Add files via upload

---
+++ database-clone/Tables/dbo.consultas.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[consultas] (
+  [id] [int] NOT NULL,
+  [usuario] [varchar](50) NOT NULL,
+  [nombre] [varchar](255) NOT NULL,
+  [apellido] [varchar](50) NOT NULL,
+  [correo] [varchar](255) NOT NULL,
+  [mensaje] [varchar](max) NOT NULL,
+  [leido] [bit] NULL,
+  [created_at] [datetime] NULL,
+  [updated_at] [datetime] NULL
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
