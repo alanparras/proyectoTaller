5a93f5ec112f9b098d2c1ad0f68f4e45e84ac5d3
alanparras <parrasemanuel@gmail.com>
2024-09-30 14:37:35 -0300

    cambios finales a capas

---
+++ CapaEntidades/Mensaje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Mensaje
+    {
+        public int id_mensaje { get; set; }
+        public string nombreCliente { get; set; }
+        public string emailCliente { get; set; }
+        public string mensaje { get; set; }
+        public bool leido { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string respuesta { get; set; }
+    }
+}
