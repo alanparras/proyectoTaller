eac565c5f05ab25cd8bd9bc2b8cb61fb0d285335
alanparras <parrasemanuel@gmail.com>
2024-09-30 14:18:03 -0300

    creacion de capas datos, entidades y negocio

---
+++ CapaDatosFinal/Perfiles_Datos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidades;
+
+namespace CapaDatos
+{
+    public class Perfiles_Datos
+    {
+        public List<Perfil> Listar()
+        {
+            List<Perfil> lista = new List<Perfil>();
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select id_perfiles, descripcion from perfiles");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            lista.Add(new Perfil()
+                            {
+                                id_perfil = Convert.ToInt32(dataReader["id_perfiles"]),
+                                descripcion = dataReader["descripcion"].ToString(),
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lista = new List<Perfil>();
+                }
+            }
+            return lista;
+        }
+    }
+}
