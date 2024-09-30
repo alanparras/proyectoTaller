bd3b4e26c11a938806cde1bb19cd53f68a8003ce
alanparras <parrasemanuel@gmail.com>
2024-09-30 14:18:03 -0300

    creacion de capas datos, entidades y negocio

---
+++ CapaEntidadesFinal/Consulta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Consulta
+    {
+        public int id_consulta { get; set; }
+        public string nombreCliente { get; set; }
+        public string apellidoCliente { get; set; }
+        public string usuarioCliente { get; set; }
+        public string clienteDomicilio { get; set; }
+        public string emailCliente { get; set; }
+        public string mensaje { get; set; }
+        public bool leido { get; set; }
+        public string created_at{ get; set; }
+        public string updated_at { get; set; }
+        public string respuesta { get; set; }
+    }
+}
---
+++ CapaNegocioFinal/Usuario_negocio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidades;
+
+namespace CapaNegocio
+{
+    public class Usuario_negocio
+    {
+        private Usuario_Datos objUser_datos = new Usuario_Datos();
+
+        public List<Usuario> Listar()
+        {
+            return objUser_datos.Listar();
+        }
+    }
+
+
+}
